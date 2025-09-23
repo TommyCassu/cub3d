c924cb285af9e53fb72e2e2d91dcc9f889cc60e7
Nonojim <nolan.pederencino@gmail.com>
2025-09-23 20:32:05 +0200

    render-raycast is norm just comm and too may fn in file

--- rendering/render_raycast.c
+++ rendering/render_raycast.c
@@ -6,29 +6,28 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 15:48:32 by tcassu            #+#    #+#             */
-/*   Updated: 2025/09/23 19:30:38 by npederen         ###   ########.fr       */
+/*   Updated: 2025/09/23 20:31:32 by npederen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void init_ray(t_data *data, int x)
+void	init_ray(t_data *data, int x)
 {
 	/* Setup ray */
 	data->game->cameraX = 2 * x / (double)RES_X - 1;
-	data->game->rayDir_x = data->map->player->dirX + data->game->planeX * data->game->cameraX;
-	data->game->rayDir_y = data->map->player->dirY + data->game->planeY * data->game->cameraX;
-
+	data->game->rayDir_x = data->map->player->dirX + data->game->planeX
+		* data->game->cameraX;
+	data->game->rayDir_y = data->map->player->dirY + data->game->planeY
+		* data->game->cameraX;
 	data->game->mapX = (int)data->map->player->x;
 	data->game->mapY = (int)data->map->player->y;
 	data->game->hit = 0;
-
 	/* calcul length of ray */
 	if (data->game->rayDir_x == 0)
 		data->game->deltaDistX = 1e30;
 	else
 		data->game->deltaDistX = fabs(1 / data->game->rayDir_x);
-
 	if (data->game->rayDir_y == 0)
 		data->game->deltaDistY = 1e30;
 	else
@@ -41,24 +40,29 @@ void	setup_angle_rayon(t_data *data)
 	if (data->game->rayDir_x < 0)
 	{
 		data->game->stepX = -1;
-		data->game->sideDistX = (data->map->player->x - data->game->mapX) * data->game->deltaDistX;
+		data->game->sideDistX = (data->map->player->x - data->game->mapX)
+			* data->game->deltaDistX;
 	}
 	else
 	{
 		data->game->stepX = 1;
-		data->game->sideDistX = (data->game->mapX + 1.0 - data->map->player->x) * data->game->deltaDistX;
+		data->game->sideDistX = (data->game->mapX + 1.0 - data->map->player->x)
+			* data->game->deltaDistX;
 	}
 	if (data->game->rayDir_y < 0)
 	{
 		data->game->stepY = -1;
-		data->game->sideDistY = (data->map->player->y - data->game->mapY) * data->game->deltaDistY;
+		data->game->sideDistY = (data->map->player->y - data->game->mapY)
+			* data->game->deltaDistY;
 	}
 	else
 	{
 		data->game->stepY = 1;
-		data->game->sideDistY = (data->game->mapY + 1.0 - data->map->player->y) * data->game->deltaDistY;
+		data->game->sideDistY = (data->game->mapY + 1.0 - data->map->player->y)
+			* data->game->deltaDistY;
 	}
 }
+
 void	dda_loop(t_data *data)
 {
 	/* ALgo DDA Loop */
@@ -76,7 +80,6 @@ void	dda_loop(t_data *data)
 			data->game->mapY += data->game->stepY;
 			data->game->side = 1;
 		}
-
 		if (data->map->map_tab[data->game->mapX][data->game->mapY] == '1')
 			data->game->hit = 1;
 	}
@@ -86,13 +89,17 @@ void	manage_draw_limits(t_data *data)
 {
 	/* Calculate the distance of the rayon from the cam position in the cam direction*/
 	if (data->game->side == 0)
-		data->game->perpWallDist = data->game->sideDistX - data->game->deltaDistX;
+		data->game->perpWallDist = data->game->sideDistX
+			- data->game->deltaDistX;
 	else
-		data->game->perpWallDist = data->game->sideDistY - data->game->deltaDistY;
+		data->game->perpWallDist = data->game->sideDistY
+			- data->game->deltaDistY;
 	/* Calcul height of line to draw colone wall */
 	data->game->lineHeight = (RES_Y / data->game->perpWallDist);
-	data->game->drawStart = -data->game->lineHeight / 2 + RES_Y / 2 + data->game->headView;
-	data->game->drawEnd = data->game->lineHeight / 2 + RES_Y / 2 + data->game->headView;
+	data->game->drawStart = -data->game->lineHeight / 2
+		+ RES_Y / 2 + data->game->headView;
+	data->game->drawEnd = data->game->lineHeight / 2
+		+ RES_Y / 2 + data->game->headView;
 	/*apply jump offset*/
 	data->game->drawStart += (int)(data->map->player->jumpoffset * RES_Y);
 	data->game->drawEnd += (int)(data->map->player->jumpoffset * RES_Y);
@@ -100,7 +107,7 @@ void	manage_draw_limits(t_data *data)
 		data->game->drawStart = 0;
 }
 
-void select_texture_side(t_game *game)
+void	select_texture_side(t_game *game)
 {
 	/* Select the good texture (NORTH/SOUTH/EAST/WEST)*/
 	if (game->side == 0)
@@ -118,28 +125,35 @@ void select_texture_side(t_game *game)
 			game->textNum = 3;
 	}
 }
+
 void	get_texture_pos(t_data *data)
 {
-	double wallX;
-	
+	double	wall_x;
+	int		jumpoffsetresy;
+
+	jumpoffsetresy = (int)(data->map->player->jumpoffset * RES_Y);
 	if (data->game->side == 0)
-		wallX = data->map->player->y + data->game->perpWallDist * data->game->rayDir_y;
+		wall_x = data->map->player->y + data->game->perpWallDist
+			* data->game->rayDir_y;
 	else
-		wallX = data->map->player->x + data->game->perpWallDist * data->game->rayDir_x;
-	wallX -= floor(wallX);
-	data->game->texX = (int)(wallX * (double)(TEXT_SIZE));
+		wall_x = data->map->player->x + data->game->perpWallDist
+			* data->game->rayDir_x;
+	wall_x -= floor(wall_x);
+	data->game->texX = (int)(wall_x * (double)(TEXT_SIZE));
 	if (data->game->side == 0 && data->game->rayDir_x > 0)
 		data->game->texX = TEXT_SIZE - data->game->texX - 1;
 	if (data->game->side == 1 && data->game->rayDir_y < 0)
 		data->game->texX = TEXT_SIZE - data->game->texX - 1;
 	data->game->step = 1.0 * TEXT_SIZE / data->game->lineHeight;
-	data->game->texPos = ((data->game->drawStart - RES_Y / 2 + data->game->lineHeight / 2 - (int)(data->map->player->jumpoffset * RES_Y) - data->game->headView)) * data->game->step;
-
+	data->game->texPos = ((data->game->drawStart - RES_Y / 2
+				+ data->game->lineHeight / 2 - jumpoffsetresy
+				- data->game->headView)) * data->game->step;
 }
-void draw_wall_col(t_data *data, int x)
+
+void	draw_wall_col(t_data *data, int x)
 {
-	int i;
-	int color;
+	int	i;
+	int	color;
 
 	color = 0xeeeeee;
 	i = data->game->drawStart;
@@ -149,10 +163,12 @@ void draw_wall_col(t_data *data, int x)
 	{
 		data->game->texY = (int)data->game->texPos & (TEXT_SIZE - 1);
 		data->game->texPos += data->game->step;
-		color = get_pixel(data->map->textdata->img[data->game->textNum], data->game->texX, data->game->texY);
+		color = get_pixel(data->map->textdata->img[data->game->textNum],
+				data->game->texX, data->game->texY);
 		if (data->game->side == 1)
 			color = (color >> 1) & 8355711;
-		if ((i < 256 && x < 256 && is_minimap_status(data, x, i) == 0) || (i >= 256 || x >= 256))
+		if ((i < 256 && x < 256 && is_minimap_status(data, x, i) == 0)
+			|| (i >= 256 || x >= 256))
 			pixels_to_image(data, x, i, color);
 		i++;
 	}
@@ -160,16 +176,19 @@ void draw_wall_col(t_data *data, int x)
 
 void	show_fps(t_data *data)
 {
+	char	fpsbuffer[32];
+
 	data->game->oldTime = data->game->time;
 	data->game->time = get_time();
 	data->game->frameTime = (data->game->time - data->game->oldTime) / 1000.0;
-	char fpsbuffer[32];
 	sprintf(fpsbuffer, "FPS %F", 1.0 / data->game->frameTime);
-	mlx_string_put(data->mlx->ptr, data->mlx->win, 500, 20, 0x000000, fpsbuffer);
+	mlx_string_put(data->mlx->ptr, data->mlx->win,
+		500, 20, 0x000000, fpsbuffer);
 }
-void render_raycast(t_data *data, t_game *game)
+
+void	render_raycast(t_data *data, t_game *game)
 {
-	int x;
+	int	x;
 
 	while (1)
 	{
@@ -187,22 +206,21 @@ void render_raycast(t_data *data, t_game *game)
 			draw_wall_col(data, x);
 			draw_ceiling(data, game, x, data->map->ceilling_rgb);
 		}
-		mlx_put_image_to_window(data->mlx->ptr, data->mlx->win, data->mlx->img->ptr, 0, 0);
+		mlx_put_image_to_window(data->mlx->ptr, data->mlx->win,
+			data->mlx->img->ptr, 0, 0);
 		show_fps(data);
 		game->moveSpeed = game->frameTime * 8.0;
 		game->rotSpeed = game->frameTime * 3.0;
-		break;
+		break ;
 	}
 }
 
-void calcul_jump_offset(t_data *data)
+void	calcul_jump_offset(t_data *data)
 {
 	if (data->map->player->isjumping)
 	{
 		data->map->player->jumpoffset += data->map->player->jumpspeed;
-
 		data->map->player->jumpspeed -= 0.001;
-
 		if (data->map->player->jumpoffset <= 0)
 		{
 			data->map->player->jumpoffset = 0;
