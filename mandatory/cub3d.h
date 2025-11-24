5281e0139ebf5f1d655866f369021f8eeb3b8e14
Nonojim <nolan.pederencino@gmail.com>
2025-11-24 16:02:11 +0100

    refactor read data in mandatory

--- mandatory/cub3d.h
+++ mandatory/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcassu <tcassu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 14:55:54 by tcassu            #+#    #+#             */
-/*   Updated: 2025/10/08 21:58:47 by tcassu           ###   ########.fr       */
+/*   Updated: 2025/11/24 16:01:14 by npederen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -197,6 +197,8 @@ int		go_right(t_data *data, t_player *player);
 int		go_left(t_data *data, t_player *player);
 void	move_head(t_game *game, t_player *player);
 /* --- reading_data --- */
+/* read_data_utils.c */
+void	read_line_gnl(t_data *data, int fd, int map_line);
 /* read_data.c */
 void	config_memory(t_data *data, char *line, char *direction);
 int		add_data(t_data *data, char *line);
